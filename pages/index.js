31d455dcab032b0351ddcda3ccccef2c9e3e52f6
PROVILLE Maximilien <maximilien.proville77@gmail.com>
2022-04-29 13:38:57 +0200

    Dashboard Posts Feed

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,70 @@
 import Head from "next/head";
-
 import styles from "../styles/Home.module.css";
-
 import Loader from "../components/Loader";
-
+import PostFeed from "../components/PostFeed";
 import toast from "react-hot-toast";
+import { firestore } from "../lib/firebase";
+import { postToJSON, fromMillis } from "../lib/firebase";
+import { useState } from "react";
 
-export default function Home() {
+const LIMIT = 1;
+
+export async function getServerSideProps(context) {
+  const postsQuery = firestore
+    .collectionGroup("posts")
+    .where("published", "==", true)
+    .orderBy("createdAt", "desc")
+    .limit(LIMIT);
+
+  const posts = (await postsQuery.get()).docs.map(postToJSON);
+
+  return {
+    props: { posts },
+  };
+}
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length - 1];
+
+    const cursor =
+      typeof last.createdAt === "number"
+        ? fromMillis(last.createdAt)
+        : last.createdAt;
+
+    const query = firestore
+      .collectionGroup("posts")
+      .where("published", "==", true)
+      .orderBy("createdAt", "desc")
+      .startAfter(cursor)
+      .limit(LIMIT);
+
+    const newPosts = (await query.get()).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  };
+
   return (
-    <div>
-      {/* <Loader show /> */}
-      <button onClick={() => toast.success("Hello Toasts")}>Toast Me</button>
-    </div>
+    <main>
+      <PostFeed posts={posts} />
+
+      {!loading && !postsEnd && (
+        <button onClick={getMorePosts}>Load More</button>
+      )}
+
+      <Loader show={loading} />
+
+      {postsEnd && "You have reached the end!"}
+    </main>
   );
 }
